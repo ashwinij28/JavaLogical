ecbfe48ea87dba79d766b564208863ca7eb9b8d1
ashwinij28 <ashwinisjagtap10@gmail.com>
2022-03-25 10:20:10 +0530

    Changes Updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import LogicalPrograms.FibonacciProgram;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        FibonacciProgram Fi=new FibonacciProgram();
+        Fi.Fibonnaci();
     }
 }
