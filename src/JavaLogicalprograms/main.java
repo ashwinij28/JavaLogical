1ff737d21f2375e491c5a2656233a67129e8b59b
ashwinij28 <ashwinisjagtap10@gmail.com>
2022-03-26 09:10:35 +0530

    Added the main

---
+++ src/JavaLogicalprograms/main.java
@@ -0,0 +1,12 @@
+package JavaLogicalprograms;
+
+public class main {
+    public static void main(String args[])
+    {
+            FibonacciProgram Fi=new FibonacciProgram();
+            Fi.Fibonnaci();
+            PerfectNumber Pn=new PerfectNumber();
+            Pn.Perfectnumber();
+    }
+
+}
